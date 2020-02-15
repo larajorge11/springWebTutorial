0ac440f6ae933b7497be2049fd501b1ed62573d2
jorgelara <Jorge Lara>
2020-02-15 14:06:57 -0500

    Jorge Lara - Create spring web project tutorial - pets grooming

---
+++ src/test/java/com/spring/web/example/springWebTutorial/SpringWebTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.web.example.springWebTutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
