743c428c8cb3f9cbc87463b953c24b87adcccec2
jorgelara <Jorge Lara>
2020-02-20 22:10:55 -0500

    Jorge Lara - Create user controller

--- src/main/java/com/spring/web/example/springWebTutorial/controller/UserController.java
+++ src/main/java/com/spring/web/example/springWebTutorial/controller/UserController.java
@@ -1,4 +1,22 @@
 package com.spring.web.example.springWebTutorial.controller;
 
+import com.spring.web.example.springWebTutorial.repository.UserRepository;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class UserController {
+
+    private final UserRepository userRepository;
+
+    public UserController(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @RequestMapping("/users")
+    public String getUsers(Model model) {
+        model.addAttribute("users", userRepository.findAll());
+        return "users";
+    }
 }
